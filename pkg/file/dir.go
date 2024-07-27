04777cf24b8bcad5514b1dd936a92147f6f69d2c
Omar <omar391@gmail.com>
2024-07-27 16:00:37 +0600

    feat: added recursive dir and file clone

--- pkg/file/dir.go
+++ pkg/file/dir.go
@@ -1,6 +1,7 @@
 package gfile
 
 import (
+	"io"
 	"os"
 	"path/filepath"
 	"sync"
@@ -33,4 +34,46 @@ func GetDirSize(path string) (int64, error) {
 
 	wg.Wait()
 	return size, nil
+}
+
+// CopyDir copies a directory from src to dst recursively.
+func CopyDir(src string, dst string) error {
+	return filepath.Walk(src, func(path string, info os.FileInfo, err error) error {
+		if err != nil {
+			return err
+		}
+
+		relPath, err := filepath.Rel(src, path)
+		if err != nil {
+			return err
+		}
+		destPath := filepath.Join(dst, relPath)
+
+		if info.IsDir() {
+			return os.MkdirAll(destPath, info.Mode())
+		}
+
+		return CopyFile(path, destPath, info.Mode())
+	})
+}
+
+// CopyFile copies a single file from src to dst and preserves file mode.
+func CopyFile(src, dst string, mode os.FileMode) error {
+	sourceFile, err := os.Open(src)
+	if err != nil {
+		return err
+	}
+	defer sourceFile.Close()
+
+	destFile, err := os.Create(dst)
+	if err != nil {
+		return err
+	}
+	defer destFile.Close()
+
+	if _, err = io.Copy(destFile, sourceFile); err != nil {
+		return err
+	}
+
+	return os.Chmod(dst, mode)
 }
--- pkg/file/dir_test.go
+++ pkg/file/dir_test.go
@@ -5,7 +5,7 @@ import (
 	"path/filepath"
 	"testing"
 
-	file_utils "github.com/omgolab/go-commons/pkg/file"
+	gfile "github.com/omgolab/go-commons/pkg/file"
 )
 
 func TestGetDirSize(t *testing.T) {
@@ -28,7 +28,7 @@ func TestGetDirSize(t *testing.T) {
 	}
 
 	// Call the function to get the actual size of the directory
-	actualSize, err := file_utils.GetDirSize(dir)
+	actualSize, err := gfile.GetDirSize(dir)
 	if err != nil {
 		t.Errorf("GetDirSize returned an error: %v", err)
 	}
@@ -40,7 +40,6 @@ func TestGetDirSize(t *testing.T) {
 }
 
 // Helper functions for creating temporary directory and files
-
 func createTempDir() string {
 	dir, err := os.MkdirTemp("", "test-dir")
 	if err != nil {
@@ -60,4 +59,76 @@ func createTestFile(dir, filename string, size int64) string {
 	_ = file.Truncate(size)
 
 	return filePath
+}
+
+func TestCopyFile(t *testing.T) {
+	srcFile, err := os.CreateTemp("", "srcFile")
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer os.Remove(srcFile.Name())
+
+	content := []byte("Hello, World!")
+	if _, err := srcFile.Write(content); err != nil {
+		t.Fatal(err)
+	}
+	srcFile.Close()
+
+	dstFile, err := os.CreateTemp("", "dstFile")
+	if err != nil {
+		t.Fatal(err)
+	}
+	dstFile.Close()
+	defer os.Remove(dstFile.Name())
+
+	if err := gfile.CopyFile(srcFile.Name(), dstFile.Name(), 0644); err != nil {
+		t.Fatal(err)
+	}
+
+	dstContent, err := os.ReadFile(dstFile.Name())
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if string(dstContent) != string(content) {
+		t.Errorf("expected %s but got %s", content, dstContent)
+	}
+}
+
+func TestCopyDir(t *testing.T) {
+	srcDir, err := os.MkdirTemp("", "srcDir")
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer os.RemoveAll(srcDir)
+
+	subDir := filepath.Join(srcDir, "subDir")
+	if err := os.Mkdir(subDir, 0755); err != nil {
+		t.Fatal(err)
+	}
+
+	srcFile := filepath.Join(subDir, "file.txt")
+	if err := os.WriteFile(srcFile, []byte("Hello, World!"), 0644); err != nil {
+		t.Fatal(err)
+	}
+
+	dstDir, err := os.MkdirTemp("", "dstDir")
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer os.RemoveAll(dstDir)
+
+	if err := gfile.CopyDir(srcDir, dstDir); err != nil {
+		t.Fatal(err)
+	}
+
+	dstFile := filepath.Join(dstDir, "subDir", "file.txt")
+	dstContent, err := os.ReadFile(dstFile)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if string(dstContent) != "Hello, World!" {
+		t.Errorf("expected %s but got %s", "Hello, World!", dstContent)
+	}
 }
